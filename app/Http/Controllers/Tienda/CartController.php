f72fff355ff2687e0fdcb550b0ac87c8ed2c4a24
luisvilca10 <martinvilca@upeu.edu.pe>
2024-05-24 00:22:12 -0500

    Cupon Tiernda

--- app/Http/Controllers/Tienda/CartController.php
+++ app/Http/Controllers/Tienda/CartController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Tienda;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Ecommerce\Cart\CartCollection;
 use App\Http\Resources\Ecommerce\Cart\CartResource;
+use App\Models\Coupon\Coupon;
 use App\Models\Sale\Cart;
 use Illuminate\Http\Request;
 
@@ -29,6 +30,77 @@ class CartController extends Controller
         //
     }
 
+    public function apply_coupon(Request $request)
+    {
+        $cupon = Coupon::where("code",$request->code)->where("state",1)->first();
+
+        if(!$cupon){
+            return response()->json(["message" => 403, "message_text" => "EL CUPON INGRESADO NO EXISTE"]);
+        }
+
+        $carts = Cart::where("user_id",auth('api')->user()->id)->get();
+
+        foreach ($carts as $key => $cart) {
+           if($cupon->type_coupon == 1){ // cupon es de tipo course
+                $is_exits_course_cupon = false;
+                foreach ($cupon->courses as $key => $course) {
+                    if($course->course_id == $cart->course_id){
+                        $is_exits_course_cupon = true;
+                        break;
+                    }
+                }
+                if($is_exits_course_cupon){
+                    $total = 0;
+                    if($cupon->type_discount == 1){//%
+                        $total = $cart->precio_unitario - $cart->precio_unitario*($cupon->discount*0.01);
+                    }
+                    if($cupon->type_discount == 2){//USD
+                        $total = $cart->precio_unitario - $cupon->discount;
+                    }
+
+                    $cart->update([
+                        "type_discount" => $cupon->type_discount,
+                        "discount" => $cupon->discount,
+                        "type_campaing" => NULL,
+                        "code_cupon" => $cupon->code,
+                        "code_discount" => NULL,
+                        "total" => $total,
+                    ]);
+                }
+           }
+           if($cupon->type_coupon == 2){ // cupon de tipo categoria
+            $is_exits_course_cupon = false;
+                foreach ($cupon->categories as $key => $categorie) {
+                    if($categorie->categorie_id == $cart->course->categorie_id){
+                        $is_exits_course_cupon = true;
+                        break;
+                    }
+                }
+                if($is_exits_course_cupon){
+                    $total = 0;
+                    if($cupon->type_discount == 1){//%
+                        $total = $cart->precio_unitario - $cart->precio_unitario*($cupon->discount*0.01);
+                    }
+                    if($cupon->type_discount == 2){//USD
+                        $total = $cart->precio_unitario - $cupon->discount;
+                    }
+
+                    $cart->update([
+                        "type_discount" => $cupon->type_discount,
+                        "discount" => $cupon->discount,
+                        "type_campaing" => NULL,
+                        "code_cupon" => $cupon->code,
+                        "code_discount" => NULL,
+                        "total" => $total,
+                    ]);
+                }
+           }
+        }
+
+        $carts = Cart::where("user_id",auth('api')->user()->id)->get();
+
+        return response()->json(["carts" => CartCollection::make($carts)]);
+    }
     /**
      * Store a newly created resource in storage.
      */
--- routes/api.php
+++ routes/api.php
@@ -71,5 +71,6 @@ Route::group([
     Route::get("home",[HomeController::class,"home"]);
     Route::get("course-detail/{slug}",[HomeController::class,"course_detail"]);
     Route::resource('/cart',CartController::class);
+    Route::post('/apply_coupon',[CartController::class, "apply_coupon"]);
 });
 
